40e6d8e1468c13d1f0e25a2bdfe218fb474e485c
ibunubi <m_aliimron@yahoo.com>
2014-06-16 17:01:49 +0700

    starq v2, slice from eps file

---
+++ js/starq.js
@@ -0,0 +1,21 @@
+$(window).scroll(function() {
+    console.log($(".navbar").offset().top);
+    if ($(".navbar").offset().top == 0) {
+        $("#scrollSpy").removeClass("when-scroll");
+    } else {
+        $("#scrollSpy").addClass("when-scroll");
+    }
+});
+
+$(document).ready(function(){
+
+    $("#scrollSpy").scrollspy();
+
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
